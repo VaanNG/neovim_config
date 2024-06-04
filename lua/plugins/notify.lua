ff09cb2450fdbeb9f5cdf6df0ccd2bc76581db1f
Van Nguyen <vanhoanganh98@gmail.com>
2024-06-05 02:25:27 +0700

    restore notify config

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -17,6 +17,24 @@ return {
     require('notify').setup(
     {
       background_colour = "#000000",
+      fps = 30,
+      icons = {
+      DEBUG = "",
+      ERROR = "",
+      INFO = "",
+      TRACE = "✎",
+      WARN = ""
+      },
+      level = 2,
+      minimum_width = 50,
+      render = "default",
+      stages = "fade_in_slide_out",
+      time_formats = {
+      notification = "%T",
+      notification_history = "%FT%T"
+      },
+      timeout = 3000,
+      top_down = true
     }
     )
     require('noice').setup({
@@ -42,7 +60,7 @@ return {
             },
           },
           popupmenu = {
-            enabled = true, -- enables the Noice popupmenu UI
+            enabled = false, -- enables the Noice popupmenu UI
             ---@type 'nui'|'cmp'
             backend = "nui", -- backend to use to show regular cmdline completions
             ---@type NoicePopupmenuItemKind|false
