70eb05873cabc35aafbe7110376bbdf3173a3558
Van Hoang Anh Nguyen <anh.nguyen62@onemount.com>
2024-06-08 12:25:19 +0700

    add search bcommit to whichkey key

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -84,24 +84,24 @@ return {
                 name = "Fuzzy Finder",
                 f = {"<cmd>FzfLua files<cr>", "Find files"},
                 F = {"<cmd>FzfLua live_grep<cr>", "Find Text"},
-                g = {"<cmd>FzfLua git_commits<cr>", "Find commits"}
+                g = {"<cmd>FzfLua git_commits<cr>", "Find commits"},
+                G = {"<cmd>FzfLua git_bcommits<cr>", "Find current file commits"},
             },
-            F = {
-                name = "Formatter",
-                p = {"<cmd>Prettier<cr>", "Prettier"}
-            },
+            -- F = {
+            --     name = "Formatter",
+            --     p = {"<cmd>Prettier<cr>", "Prettier"}
+            -- },
             g = {
                 name = "Git",
-                g = {"<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit"},
                 j = {"<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk"},
                 k = {"<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk"},
-                l = {"<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame"},
+                b = {"<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame"},
                 p = {"<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk"},
                 r = {"<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk"},
                 R = {"<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer"},
                 s = {"<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk"},
                 u = {"<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>", "Undo Stage Hunk"},
-                d = {"<cmd>Gitsigns diffthis HEAD<cr>", "Diff"}
+                d = {"<cmd>Gitsigns diffthis HEAD<cr>", "Diff"},
             },
             l = {
                 name = "LSP",
