752c46c39caecd640d133c663c7324358857beff
Van Nguyen <vanhoanganh98@gmail.com>
2025-02-10 23:40:06 +0700

    update fzf-lua options according to new formats + re-add disable gitignore in files search + some obsidian updates

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -34,7 +34,7 @@ return {
                         flip_columns = 120, -- #cols to switch to horizontal on flex
                         -- Only used with the builtin previewer:
                         title = true, -- preview border title (file/buf)?
-                        title_align = "left", -- left|center|right, title alignment
+                        title_pos = "left", -- left|center|right, title alignment
                         scrollbar = "float", -- `false` or string:'float|border'
                         -- float:  in-window floating border
                         -- border: in-border chars (see below)
@@ -205,31 +205,48 @@ return {
                 },
                 -- provider setup
                 files = {
-                    -- previewer      = "bat",          -- uncomment to override previewer
-                    -- (name from 'previewers' table)
-                    -- set to 'false' to disable
-                    prompt = "Files❯ ",
-                    multiprocess = true, -- run command in a separate process
-                    git_icons = true, -- show git icons?
-                    file_icons = true, -- show file icons?
-                    color_icons = true, -- colorize file|git icons
-                    -- path_shorten   = 1,              -- 'true' or number, shorten path?
-                    -- executed command priority is 'cmd' (if exists)
-                    -- otherwise auto-detect prioritizes `fd`:`rg`:`find`
-                    -- default options are controlled by 'fd|rg|find|_opts'
-                    -- NOTE: 'find -printf' requires GNU find
-                    -- cmd            = "find . -type f -printf '%P\n'",
-                    find_opts = [[-type f -not -path '*/\.git/*' -printf '%P\n']],
-                    rg_opts = "--color=never --files --hidden --follow -g '!.git'",
-                    fd_opts = "--color=never --type f --hidden --follow --exclude .git",
+                -- previewer      = "bat",          -- uncomment to override previewer
+                                                    -- (name from 'previewers' table)
+                                                    -- set to 'false' to disable
+                prompt            = 'Files❯ ',
+                multiprocess      = true,           -- run command in a separate process
+                git_icons         = true,          -- show git icons?
+                file_icons        = true,           -- show file icons (true|"devicons"|"mini")?
+                color_icons       = true,           -- colorize file|git icons
+                -- path_shorten   = 1,              -- 'true' or number, shorten path?
+                -- Uncomment for custom vscode-like formatter where the filename is first:
+                -- e.g. "fzf-lua/previewer/fzf.lua" => "fzf.lua previewer/fzf-lua"
+                -- formatter      = "path.filename_first",
+                -- executed command priority is 'cmd' (if exists)
+                -- otherwise auto-detect prioritizes `fd`:`rg`:`find`
+                -- default options are controlled by 'fd|rg|find|_opts'
+                -- cmd            = "rg --files",
+                find_opts         = [[-type f -not -path '*/\.git/*']],
+                rg_opts           = [[--color=never --hidden --files -g "!.git"]],
+                fd_opts           = [[--color=never --hidden --type f --type l --exclude .git]],
+                dir_opts          = [[/s/b/a:-d]],
+                -- by default, cwd appears in the header only if {opts} contain a cwd
+                -- parameter to a different folder than the current working directory
+                -- uncomment if you wish to force display of the cwd as part of the
+                -- query prompt string (fzf.vim style), header line or both
+                -- cwd_header = true,
+                cwd_prompt             = true,
+                cwd_prompt_shorten_len = 32,        -- shorten prompt beyond this length
+                cwd_prompt_shorten_val = 1,         -- shortened path parts length
+                toggle_ignore_flag = "--no-ignore", -- flag toggled in `actions.toggle_ignore`
+                toggle_hidden_flag = "--hidden",    -- flag toggled in `actions.toggle_hidden`
+                toggle_follow_flag = "-L",          -- flag toggled in `actions.toggle_follow`
+                hidden             = true,          -- enable hidden files by default
+                follow             = false,         -- do not follow symlinks by default
+                no_ignore          = false,         -- respect ".gitignore"  by default
                     actions = {
-                        -- inherits from 'actions.files', here we can override
-                        -- or set bind to 'false' to disable a default action
-                        ["default"] = actions.file_edit,
-                        -- custom actions are available too
-                        ["ctrl-y"] = function(selected)
-                            print(selected[1])
-                        end
+                      -- inherits from 'actions.files', here we can override
+                      -- or set bind to 'false' to disable a default action
+                      -- uncomment to override `actions.file_edit_or_qf`
+                      --   ["enter"]     = actions.file_edit,
+                      -- custom actions are available too
+                      --   ["ctrl-y"]    = function(selected) print(selected[1]) end,
+                        ["ctrl-r"] = {actions.toggle_ignore}
                     }
                 },
                 git = {
